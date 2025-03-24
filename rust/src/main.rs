c4e656e79a336c9c77620054f2ad4e2cf5b00e0b
Roger D <roger.delacruz.jr@gmail.com>
2025-03-24 01:48:29 -0400

    Added command line arguments to Rust benchmarking

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,15 +2,39 @@ mod benchmarkers;
 mod errors;
 mod operations;
 
+use clap::Parser;
+
 use crate::benchmarkers::base_benchmarker::BaseBenchmarker;
 use crate::benchmarkers::quick_sort_benchmarker::QuickSortBenchmarker;
 use crate::errors::BenchmarkerError;
 
-const LOOP_COUNT: u32 = 1000;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    operation: String,
+
+    #[arg(short, long)]
+    input_file: String,
+
+    /// The count of operations to perform (default: 1000)
+    #[arg(short, long, default_value_t = 1000)]
+    count: u32,
+
+    /// Whether to verify the operation (default: false)
+    #[arg(short, long, default_value_t = false)]
+    verify: bool,
+}
 
 // cargo build --release
-// ./target/release/rust
+// ./target/release/rust -o quick_sort -i ../inputs/random.json -c 1000 -v
 fn main() -> Result<(), BenchmarkerError> {
-    let quick_sort_benchmarker = QuickSortBenchmarker::new("../inputs/random.json");
-    quick_sort_benchmarker.print_benchmark_analysis(LOOP_COUNT, true)
+    let args = Args::parse();
+
+    let benchmarker = match args.operation.as_str() {
+        "quick_sort" => QuickSortBenchmarker::new(&args.input_file),
+        _ => panic!("Invalid operation"),
+    };
+
+    benchmarker.print_benchmark_analysis(args.count, args.verify)
 }
