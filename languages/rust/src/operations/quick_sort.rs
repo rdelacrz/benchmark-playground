8b5c4426157ad7ecd0444682a4f2b1032f8d27c1
rdelacrz <roger.delacruz.jr@gmail.com>
2025-04-09 00:25:30 -0400

    Added integers test to Rust

--- languages/rust/src/operations/quick_sort.rs
+++ languages/rust/src/operations/quick_sort.rs
@@ -25,3 +25,85 @@ pub fn quick_sort<T: Ord>(arr: &mut [T]) -> &mut [T] {
     }
     arr
 }
+
+/*******************************************************
+ * Unit Tests
+ ******************************************************/
+
+#[test]
+fn test_quick_sort_zero_elements() {
+    // Arrange
+    let mut unsorted: Vec<i32> = Vec::new();
+    let expected_result: Vec<i32> = Vec::new();
+
+    // Act
+    let actual_result = quick_sort(&mut unsorted);
+
+    // Assert
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn test_quick_sort_single_element() {
+    // Arrange
+    let mut unsorted: [&str; 1] = ["badger"];
+    let expected_result: [&str; 1] = ["badger"];
+
+    // Act
+    let actual_result = quick_sort(&mut unsorted);
+
+    // Assert
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn test_quick_sort_multiple_odd_elements() {
+    // Arrange
+    let mut unsorted: [&str; 9] = ["zinc", "trap", "tree", "trait", "badger", "apple", "chemistry", "pig", "zoo"];
+    let expected_result: [&str; 9] = ["apple", "badger", "chemistry", "pig", "trait", "trap", "tree", "zinc", "zoo"];
+
+    // Act
+    let actual_result = quick_sort(&mut unsorted);
+
+    // Assert
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn test_quick_sort_multiple_even_elements() {
+    // Arrange
+    let mut unsorted: [&str; 10] = ["trap", "apple", "chemistry", "pig", "zoo", "tree", "trait", "badger", "zinc", "crack"];
+    let expected_result: [&str; 10] = ["apple", "badger", "chemistry", "crack", "pig", "trait", "trap", "tree", "zinc", "zoo"];
+
+    // Act
+    let actual_result = quick_sort(&mut unsorted);
+
+    // Assert
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn test_quick_sort_already_sorted() {
+    // Arrange
+    let mut unsorted: [&str; 10] = ["apple", "badger", "chemistry", "crack", "pig", "trait", "trap", "tree", "zinc", "zoo"];
+    let expected_result: [&str; 10] = ["apple", "badger", "chemistry", "crack", "pig", "trait", "trap", "tree", "zinc", "zoo"];
+
+    // Act
+    let actual_result = quick_sort(&mut unsorted);
+
+    // Assert
+    assert_eq!(expected_result, actual_result);
+}
+
+#[test]
+fn test_quick_sort_integers() {
+    // Arrange
+    let mut unsorted: [i16; 9] = [4, -54, 40, 400, 2, -7, 0, 1, 4];
+    let expected_result: [i16; 9] = [-54, -7, 0, 1, 2, 4, 4, 40, 400];
+
+    // Act
+    let actual_result = quick_sort(&mut unsorted);
+
+    // Assert
+    assert_eq!(expected_result, actual_result);
+}
